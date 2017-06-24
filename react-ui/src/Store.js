62e6ac3cc261a06382f97b95c7940d65a11ab116
David Espinoza <despinoza1027@gmail.com>
2017-06-24 00:37:38 -0500

    added redux dev tool support

--- react-ui/src/Store.js
+++ react-ui/src/Store.js
@@ -25,7 +25,7 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export {
   actions, store
